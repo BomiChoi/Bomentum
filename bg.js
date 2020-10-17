636551c4925f544b0d91f5c0548f8541503954df
Bomi Choi <hibomi97@gmail.com>
2020-10-18 00:41:31 +0900

    added image preloading

--- bg.js
+++ bg.js
@@ -9,18 +9,47 @@ function paintImage() {
     // 새벽
     random = genRandom(5);
     src = `img/dawn${random + 1}.jpg`;
+
+    let preloadImage;
+    window.onload = function() {
+      for(i = 1; i <= 5; i++) {
+        preloadImage  = new Image();
+        preloadImage.src=`img/dawn${i}.jpg`;
+      }
+    }
   } else if (hour >= 8 && hour < 17) {
     //낮
     random = genRandom(6);
     src = `img/day${random + 1}.jpg`;
+    let preloadImage;
+    window.onload = function() {
+      for(i = 1; i <= 6; i++) {
+        preloadImage  = new Image();
+        preloadImage.src=`img/day${i}.jpg`;
+      }
+    }
   } else if (hour >= 17 && hour < 20) {
     // 저녁
     random = genRandom(3);
     src = `img/evening${random + 1}.jpg`;
+    let preloadImage;
+    window.onload = function() {
+      for(i = 1; i <= 3; i++) {
+        preloadImage  = new Image();
+        preloadImage.src=`img/evening${i}.jpg`;
+      }
+    }
   } else {
     // 밤
     random = genRandom(4);
     src = `img/night${random + 1}.jpg`;
+    let preloadImage;
+    window.onload = function() {
+      for(i = 1; i <= 4; i++) {
+        preloadImage  = new Image();
+        preloadImage.src=`img/night${i}.jpg`;
+      }
+    }
   }
   image.style.backgroundImage = `url("${src}")`;
 
